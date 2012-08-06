b2a84d5f9f3760ff1f93ed13f6fbb82a2fed7c5c
Tyler Goodlet <tgoodlet@gmail.com>
2012-08-06 02:23:52 -0400

    change bash scripts to use sw,ts of 4, edit tmux to use dim status bar

--- bin/test.sh
+++ bin/test.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # script to test exit codes with command substitution
 
-for f in ( ./* dogs.txt ); do 
-	
+for f in ( ./* dogs.txt ); do
+
     declare err=$(cat ${f}) || exit $? && echo command substitution failed
     #echo last err is = $?
 
     if [[ $? == 0 ]]; then
-	echo inside if!
-	echo ${f}
-	echo "${err}"
+        echo inside if!
+        echo ${f}
+        echo "${err}"
     fi
     echo made it to the end!
 done
