08c1ec95c38f24d45f5f943e1c835433252b0685
Tyler Goodlet <tgoodlet@gmail.com>
2012-12-19 01:54:53 -0500

    add zsh_snippets with additions from simon, update scripts/bin directory

---
+++ dotrc/zsh/zsh_snippets/prompt.zsh
@@ -0,0 +1,103 @@
+# prompt settings
+
+# options
+setopt prompt_subst
+autoload -U vcs_info
+autoload -U colors && colors
+
+# vcs style settings
+zstyle ':vcs_info:*' enable git cvs svn
+zstyle ':vcs_info:*' actionformats '%F{5}(%f%s%F{5})%F{3}-%F{5}[%F{2}%b%F{3}|%F{1}%a%F{5}]%f '
+zstyle ':vcs_info:*' formats '%F{5}(%f%s%F{5})%F{3}-%F{5}[%F{2}%b%F{5}]%f '
+zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{1}:%F{3}%r'
+
+vimode=I
+# set vimode to current editing mode
+function zle-line-init zle-keymap-select {
+    vimode="${${KEYMAP/vicmd/C}/(main|viins)/}"
+    zle reset-prompt
+}
+zle -N zle-keymap-select
+zle -N zle-line-init
+
+function vcs_info_wrapper {
+    vcs_info
+    [[ -n "$vcs_info_msg_0_" ]] && echo " %{$fg[grey]%}${vcs_info_msg_0_/ /}%{$reset_color%}"
+}
+
+
+# RPROMPT pwd updating
+function precmd {
+        # only update right-prompt on dir change
+        if [[ ${cur_dir} != ${PWD} ]]; then
+            set_rprompt
+            unset set_rprompt
+            cur_dir=${PWD}
+        else
+            RPROMPT=""
+        fi
+        echo    # add a new line to each output
+}
+
+# RPROMPT="%{$reset_color%}[%{$fg[green]%}%~%{$reset_color%}]"
+function set_rprompt {
+    local pmt="red"
+    (( EUID == 0 )) && pmt="blue"
+    [[ -n $SSH_CONNECTION ]] && pmt="magenta"
+    # PROMPT="┌┤%{$fg[green]%}%n%{$reset_color%}>%{$fg[${pmt}]%}%m%{$reset_color%} %{$fg[blue]%}[%0~]%{$reset_color%}
+    # └→ "
+
+    RPROMPT='$(vcs_info_wrapper)'
+    RPROMPT+=" %{$reset_color%}[%{$fg[green]%}%~%{$reset_color%}]"
+    # RPROMPT+='%{$fg[yellow]%}%(?.. %?)%{$reset_color%}'
+    # RPROMPT+='%{$fg[red]%} ${vimode} %{$reset_color%}'
+}
+set_rprompt
+unset set_rprompt
+
+# left prompt
+pmt="green"
+PROMPT="%{$fg[${pmt}]%}->%#%{$reset_color%}"
+
+# use cursor as indicator of vi mode
+cmd_mode_col="red"
+cursor_col="grey"
+
+zle-keymap-select () {
+    if [ $KEYMAP = vicmd ]; then
+        if [[ $TMUX = '' ]]; then
+            echo -ne "\033]12;${cmd_mode_col}\007"
+        else
+            printf "\033Ptmux;\033\033]12;${cmd_mode_col}\007\033\\"
+        fi
+    else
+        if [[ $TMUX = '' ]]; then
+            echo -ne "\033]12;${cursor_col}\007"
+        else
+            printf "\033Ptmux;\033\033]12;${cursor_col}\007\033\\"
+        fi
+    fi
+}
+
+zle-line-init () {
+    zle -K viins
+    echo -ne "\033]12;${cursor_col}\007"
+}
+
+# insert sudo to bol on ALT-s
+insert_sudo () {
+    zle beginning-of-line
+    zle -U "sudo "
+}
+zle -N insert-sudo insert_sudo
+bindkey "^[s" insert-sudo
+
+# Show "waiting dots" while something tab-completes
+# (found on zsh-users)
+expand-or-complete-with-dots() {
+    echo -n "\e[31m...\e[0m"
+    zle expand-or-complete
+    zle redisplay
+}
+zle -N expand-or-complete-with-dots
+bindkey "^I" expand-or-complete-with-dots
---
+++ dotrc/zsh/zsh_snippets/tmux.zsh
@@ -0,0 +1,37 @@
+# TMUX detection
+
+# check if tmux is installed
+if type tmux 2>&1 >/dev/null; then
+
+    # if NOT inside a session AND no server yet exits, start a new server and new session
+    if  (test -z ${TMUX} && [[ -z $(pidof tmux) ]]); then
+
+        echo 'starting new tmux server'
+
+        # NOTE you must create a session (eg. new -s main zsh)
+        # in ~/.tmux.conf or this attach line will fail if no prior 
+        # session/server exists 
+        tmux attach && exit
+
+        echo ':exit shell as well? [Y/n]'
+        read input
+        if [[ $input == ( y || \n ) ]]; then
+            exit 
+        fi
+
+    # if session exists then create a new window and attach to it
+    # (this code is called whenever a new shell is opened outside tmux
+    # and the tmux session 'main' already exists)
+    elif (test -z ${TMUX}) && $(tmux has-session -t main); then
+
+        echo 'trying to connect to main session'
+        if (test -z $(tmux list-clients -t main)); then
+            tmux attach && exit
+        else
+            tmux new-session zsh && exit
+        fi
+    elif (test -n ${TMUX}); then
+        echo 'new tmux pseudo-shell'
+    fi
+    [[ -z $TMUX ]] && echo 'This is outside a tmux shell! Think of all the potential you waste...'
+fi
