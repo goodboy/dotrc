1856a8624641cc6c3be5d32a75393c3166f6bf06
Tyler Goodlet <tgoodlet@gmail.com>
2015-01-05 02:16:45 -0500

    Zsh prompt enhancements :D
    - get vi-mode indication by cursor colour working by adding code
      from the blog post here:
      http://ivyl.0xcafe.eu/2013/02/03/refining-zsh/#vi_mode_status_indicator
    - add some other stuff related to vcs prompts from this post but leave
      it commented for now
    - always update the RPROMPT, not just on dir changes
    - add `user@hostname: pwd` printing to the xterm title
    - change prompt to more minimalist `>>>`
    - remove ALT-s sudo prefix hot key

--- dotrc/zsh/zsh_snippets/prompt.zsh
+++ dotrc/zsh/zsh_snippets/prompt.zsh
@@ -11,6 +11,12 @@ zstyle ':vcs_info:*' actionformats '%F{5}(%f%s%F{5})%F{3}-%F{5}[%F{2}%b%F{3}|%F{
 zstyle ':vcs_info:*' formats '%F{5}(%f%s%F{5})%F{3}-%F{5}[%F{2}%b%F{5}]%f '
 zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{1}:%F{3}%r'
 
+# zstyle ':vcs_info:*' stagedstr "${fg_blue}?"
+# zstyle ':vcs_info:*' unstagedstr "${fg_brown}?"
+# zstyle ':vcs_info:*' check-for-changes true
+# zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{1}:%F{11}%r'
+# zstyle ':vcs_info:*' enable git svn
+
 # vimode=I
 # # set vimode to current editing mode
 # function zle-line-init zle-keymap-select {
@@ -19,34 +25,37 @@ zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{1}:%F{3}%r'
 # }
 
 function vcs_info_wrapper {
-    vcs_info
-   [[ -n "$vcs_info_msg_0_" ]] && echo " %{$fg[grey]%}${vcs_info_msg_0_/ /}%{$reset_color%}"
+   vcs_info
+   [[ -n "$vcs_info_msg_0_" ]] && echo " %{$fg[gray]%}${vcs_info_msg_0_/ /}%{$reset_color%}"
 }
 
-# RPROMPT pwd updating
 function precmd {
-        # only update right-prompt on dir change
-        if [[ ${cur_dir} != ${PWD} ]]; then
-            set_rprompt
-            unset set_rprompt
-            cur_dir=${PWD}
-        else
-            unset RPROMPT
-            # RPROMPT=""
-        fi
-        echo    # add a new line to each output
+    # if [[ -z $(git ls-files --other --exclude-standard 2> /dev/null) ]] {
+    #     zstyle ':vcs_info:*' formats "${at_normal} ${fg_dgray}%b%c%u${at_normal}"
+    # } else {
+    #     zstyle ':vcs_info:*' formats "${at_normal} ${fg_dgray}%b%c%u${fg_red}!${at_normal}"
+    # }
+    # vcs_info
+
+    echo  # add a new line to each output
+    # update the xterm title with `username@hostname pwd`
+    case $TERM in
+        xterm*)
+            print -Pn "\e]0;%n@%m: %~\a";;
+    esac
 }
+
+# enable later substitution in prompt
+# setopt PROMPT_SUBST
 
 # RPROMPT="%{$reset_color%}[%{$fg[green]%}%~%{$reset_color%}]"
 function set_rprompt {
     local pmt="red"
     (( EUID == 0 )) && pmt="blue"
     [[ -n $SSH_CONNECTION ]] && pmt="magenta"
-    # PROMPT="┌┤%{$fg[green]%}%n%{$reset_color%}>%{$fg[${pmt}]%}%m%{$reset_color%} %{$fg[blue]%}[%0~]%{$reset_color%}
-    # └→ "
-
-    RPROMPT=" %{$reset_color%}[%{$fg[green]%}%~%{$reset_color%}]"
-    RPROMPT+='$(vcs_info_wrapper)'
+    # RPROMPT="%{$reset_color%}%(?/${at_normal}/${fg_red})%%${at_normal}"
+    RPROMPT='$(vcs_info_wrapper)'
+    # RPROMPT+="%c \${vcs_info_msg_0_}"
     # RPROMPT+='%{$fg[yellow]%}%(?.. %?)%{$reset_color%}'
     # RPROMPT+='%{$fg[red]%} ${vimode} %{$reset_color%}'
 }
@@ -55,46 +64,45 @@ unset set_rprompt
 
 # left prompt
 pmt="green"
-PROMPT="%{$fg[${pmt}]%}->%#%{$reset_color%}"
+PROMPT="%{$fg[${pmt}]%} >>> %{$reset_color%}"
 
 # use cursor as indicator of vi mode
-cmd_mode_col="red"
-cursor_col="grey"
+# urxvt (and family) accepts even #RRGGBB
+INSERT_PROMPT="gray"
+COMMAND_PROMPT="red"
 
-zle-keymap-select () {
-    if [ $KEYMAP = vicmd ]; then
-        if [[ $TMUX = '' ]]; then
-           echo -ne "\033]12;${cmd_mode_col}\007"
-
-        elif [[ $TERM = 'screen-256color' ]]; then
-           printf "\033Ptmux;\033\033]12;${cmd_mode_col}\007\033\\"
-        fi
+# helper for setting color including all kinds of terminals
+set_prompt_color() {
+    if [[ $TERM = "linux" ]]; then
+        # nothing
+    elif [[ $TMUX != '' ]]; then
+        printf '\033Ptmux;\033\033]12;%b\007\033\\' "$1"
     else
-        if [[ $TMUX = '' ]]; then
-           echo -ne "\033]12;${cursor_col}\007"
-
-        elif [[ $TERM = 'screen-256color' ]]; then
-           printf "\033Ptmux;\033\033]12;${cursor_col}\007\033\\"
-        fi
+        echo -ne "\033]12;$1\007"
     fi
 }
-# zle -N zle-keymap-select
 
-# zle-line-init () {
-#    zle -K viins
-#    if [[ $TERM = 'screen-256color' ]]; then
-#        echo -ne "\033]12;${cursor_col}\007"
-#    fi
-# }
-# zle -N zle-line-init
-
-# insert sudo to bol on ALT-s
-insert_sudo () {
-    zle beginning-of-line
-    zle -U "sudo "
+# change cursor color based on vi mode
+zle-keymap-select () {
+    if [ $KEYMAP = vicmd ]; then
+        set_prompt_color $COMMAND_PROMPT
+    else
+        set_prompt_color $INSERT_PROMPT
+    fi
 }
-zle -N insert-sudo insert_sudo
-bindkey "^[s" insert-sudo
+
+zle-line-finish() {
+    set_prompt_color $INSERT_PROMPT
+}
+
+zle-line-init () {
+    zle -K viins
+    set_prompt_color $INSERT_PROMPT
+}
+
+zle -N zle-keymap-select
+zle -N zle-line-init
+zle -N zle-line-finish
 
 # Show "waiting dots" while something tab-completes
 # (found on zsh-users)
@@ -104,4 +112,6 @@ expand-or-complete-with-dots() {
     zle redisplay
 }
 zle -N expand-or-complete-with-dots
+
+# map tab key to call the above routine
 bindkey "^I" expand-or-complete-with-dots
